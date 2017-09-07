f0b3b54bbe511209ab8ed401c3365cb2c2fc3a55
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-07 16:42:46 +0200

    finished ok url encoded %2b for caractere : + and %2F for caracter : /

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ let app = express();
 
 //Crée un chemin et lui donner un parametre dans l'url comme : ?a=1&b=2 et il fera son calcule
 app.get('/add', function(req, res) {
-  let a = parseFloat(req.query.a); //transforme ce que je rentre en parametre en nombre.
+  let a = parseFloat(req.query.a); //transforme une chaine de caractere en un nombre flotant ( ceux a virgule).
   let b = parseFloat(req.query.b);
   let op = req.query.op; //opérateur
 
   let result;
   if (op === "-") {
-    result = (a - b).toString();
-  }else if (op === "p" ) {
+    result = (a - b).toString(); // renvoie une chaine de carectere representant l'objet.
+  }else if (op === "+" ) { //%2B pour echapper un caractere encoder dans l'url.
     result = (a + b ).toString();
   }else if (op === "*" ) {
     result = (a * b ).toString();
-  }else if (op === "%" ) {
+  }else if (op === "/" ) { //%2F ""
     result = (a / b ).toString();
   }
   console.log(op);
